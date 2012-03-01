b3b0f2559c108458a995dbd8d670dfe69690c0fa
ML User <mluser@lenovo.(none)>
2012-03-01 11:51:14 +0000

    jmatio in local repo

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject jvml "0.2"
+  :repositories [["local" ~(str (.toURI (java.io.File. "maven_repository")))]
+                 ["dev.davidsoergel.com releases" "http://dev.davidsoergel.com/artifactory/repo"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [incanter "1.3.0"]])
+                 [incanter "1.3.0"]
+                 [com.jmatio/jmatio "071005"]
+                 [edu.berkeley.compbio/jlibsvm "0.902"]])
