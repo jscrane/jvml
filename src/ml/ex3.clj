19c71eb685cae3644057acfe076247978b948dea
jscrane <alqpal123>
2013-05-13 16:28:23 +0100

    cleanup code a bit

--- src/ml/ex3.clj
+++ src/ml/ex3.clj
@@ -6,14 +6,12 @@
 
 (defn one-vs-all [X y num-labels lambda iters]
   (let [m (nrow X) X (add-intercept X) initial-theta (zeroes (ncol X))]
-    (reduce (fn [all-theta c]
-              (let [samples (into [] (map #(if (= % c) 1 0) y))
-                    theta (gradient-descent (reg-cost-fn logistic-hypothesis X samples lambda) initial-theta :num-iters iters)]
-                (println c (logistic-cost X samples theta))
-                (conj all-theta theta)))
-      [] (range 1 (inc num-labels)))))
+    (apply bind-rows
+      (for [c (range 1 (inc num-labels))]
+        (let [samples (into [] (map #(if (= % c) 1 0) y))]
+          (gradient-descent (reg-cost-fn logistic-hypothesis X samples lambda) initial-theta :num-iters iters))))))
 
-(defn  one-vs-all-accuracy [lambda iters]
+(defn one-vs-all-accuracy [lambda iters]
   (let [y (map int (d :y ))
         all-theta (matrix (one-vs-all (d :X ) y 10 lambda iters))
         s (mmult (add-intercept (d :X )) (trans all-theta))]
