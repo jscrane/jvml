94443d3a76c58c1d58a42aeaba8ee4d846fa8b6d
jscrane <alqpal123>
2013-05-21 12:51:06 +0100

    optimize inner loop a bit

--- src/ml/ex4.clj
+++ src/ml/ex4.clj
@@ -13,15 +13,16 @@
 (defn neural-net-cost-fn [X Y lambda]
   (let [m (nrow Y) a1 (add-intercept X) m1Y (minus 1 Y)]
     (fn [[Theta1 Theta2]]
-      (let [z2 (mmult a1 (trans Theta1))
+      (let [z2 (mmult a1 (trans Theta1))    ; 300ms! (400ms if (mmult Theta1 (trans a1)))
             a2 (add-intercept (sigmoid z2))
             z3 (mmult a2 (trans Theta2))
             a3 (sigmoid z3)
-            cost (sum (map sum (plus (mult Y (log a3)) (mult m1Y (log (minus 1 a3))))))
+;            cost (sum (map sum (plus (mult Y (log a3)) (mult m1Y (log (minus 1 a3))))))   ; 3000ms
+            cost (.zSum (plus (mult Y (log a3)) (mult m1Y (log (minus 1 a3)))))   ; 26ms
             d3 (minus a3 Y)
             d2 (mult (sel (mmult d3 Theta2) :except-cols 0) (sigmoid-gradient z2))
             delta2 (mmult (trans d3) a2)
-            delta1 (mmult (trans d2) a1)
+            delta1 (mmult (trans d2) a1)    ; 900ms!
             Theta1-reg (zero-first-column Theta1)
             Theta2-reg (zero-first-column Theta2)
             reg (* lambda 0.5 (+ (sum (map sum-of-squares Theta1-reg)) (sum (map sum-of-squares Theta2-reg))))
@@ -39,19 +40,20 @@
         b (logistic-hypothesis (trans theta2) (add-intercept a))]
     (map max-index b)))
 
-(defn random-seq [epsilon]
-  (let [r (fn [_] (- (* 2 epsilon (Math/random)) epsilon))]
-    (iterate r (r 0))))
-
 (defn random-matrix [[nrow ncol] epsilon]
-  (let [rand (random-seq epsilon)]
-    (matrix (partition ncol (take (* nrow ncol) rand)))))
+  (let [r (fn [_] (- (* 2 epsilon (Math/random)) epsilon))]
+    (matrix (partition ncol (take (* nrow ncol) (iterate r (r 0)))))))
 
+; iter  cost  predict time
+; 50    1.45  82.7%   67
+; 100   0.94  89.5%   127
+; 200   0.60  93.4%   262
+; 400   0.52  95.1%   502
+; 800   0.45  96.3%   1030
 (comment
   (time
-    (let [eps 0.12
+    (let [eps 0.25
           T1 (random-matrix (dim (:Theta1 W)) eps)
           T2 (random-matrix (dim (:Theta2 W)) eps)
-          [Theta1 Theta2] (gradient-descent (neural-net-cost-fn X Y 0.1) [T1 T2] :num-iters 50 :alpha 2.25)]
-      ;    (println "theta1" Theta1)
+          [Theta1 Theta2] (gradient-descent (neural-net-cost-fn X Y 1.0) [T1 T2] :num-iters 50 :alpha 2.25)]
       (println "predict" (double (accuracy (predict Theta1 Theta2 X) y))))))
