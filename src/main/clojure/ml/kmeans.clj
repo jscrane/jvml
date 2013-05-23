b24b76487959cbab592b3e562ff5d8485ef51576
jscrane <alqpal123>
2013-05-23 14:03:15 +0100

    - speedups

--- src/main/clojure/ml/kmeans.clj
+++ src/main/clojure/ml/kmeans.clj
@@ -2,17 +2,14 @@
   (:use (ml util)
         (incanter core stats)))
 
-(defn find-closest-centroid [v centroids]
-  (let [dv (map #(minus v %) centroids)
-        d (map #(mmult % (trans %)) dv)]
-    (indexes-of? < d)))
+(defn find-closest-centroid [point centroids]
+  (indexes-of? < (map #(sum-of-squares (minus point %)) centroids)))
 
-; this takes 9s per invocation!
 (defn find-closest-centroids [X centroids]
-  (map #(find-closest-centroid % centroids) X))
+  (map #(find-closest-centroid % centroids) (to-list X)))
 
 (defn compute-centroid [X idx k]
-  (map mean (trans (remove nil? (map #(if (= k %2) %1) X idx)))))
+  (map mean (trans (remove nil? (map #(if (= k %2) %1) (to-list X) idx)))))
 
 (defn compute-centroids [X idx k]
   (matrix (map #(compute-centroid X idx %) (range k))))
@@ -23,9 +20,7 @@
     (compute-centroids X idx k)))
 
 (defn run-kmeans [X initial-centroids n]
-  (first (drop n (iterate #(kmeans X %) (matrix initial-centroids)))))
+  (first (drop n (iterate #(kmeans X %) initial-centroids))))
 
 (defn init-centroids [X k]
   (sel X :rows (take k (permute (range (nrow X))))))
-
-
