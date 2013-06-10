37fecf9ba61f8affffa363c7fda629baa7621cb0
jscrane <alqpal123>
2013-06-10 17:11:08 +0100

    small changes to ex7a

--- src/main/clojure/ml/ex7a.clj
+++ src/main/clojure/ml/ex7a.clj
@@ -2,11 +2,14 @@
   (:use (ml matlab kmeans)
         (incanter core charts)))
 
+(defn init-ex7 []
+  (assoc (read-dataset-mat5 "data/ex7data2.mat")
+    :centroids (matrix [[3 3] [6 2] [8 5]])))
+
 (if *command-line-args*
-  (let [X (:X (read-dataset-mat5 "data/ex7data2.mat"))
-        centroids (run-kmeans X [[3 3] [6 2] [8 5]] 10)
-        idx (find-closest-centroids X centroids)]
+  (let [{:keys [centroids X]} (init-ex7)
+        idx (find-closest-centroids X (run-kmeans X centroids 10))]
     (doto
-      (scatter-plot (sel X :cols 0) (sel X :cols 1) :group-by idx)
+      (scatter-plot (sel X :cols 0) (sel X :cols 1) :x-label "" :y-label "" :group-by idx)
       (add-points (map first centroids) (map second centroids))
       (view))))
--- test/ml/test_kmeans.clj
+++ test/ml/test_kmeans.clj
@@ -1,14 +1,10 @@
 (ns ml.test-kmeans
   (:use (clojure test)
         (incanter core)
-        (ml matlab kmeans testutil)))
-
-(defn init []
-  (assoc (read-dataset-mat5 "data/ex7data2.mat")
-    :centroids (matrix [[3 3] [6 2] [8 5]])))
+        (ml matlab kmeans testutil ex7a)))
 
 (deftest test-find-closest-centroids
-  (let [{:keys [centroids X]} (init)]
+  (let [{:keys [centroids X]} (init-ex7)]
     (is (= 0 (find-closest-centroid (trans [1.8421 4.6076]) centroids)))
     (is (= 2 (find-closest-centroid (trans [5.6586 4.8000]) centroids)))
     (is (= 1 (find-closest-centroid (trans [6.3526 3.2909]) centroids)))
@@ -16,7 +12,7 @@
 
 (deftest test-compute-centroids
   (let [approx (approximately 1e-6)
-        {:keys [centroids X]} (init)
+        {:keys [centroids X]} (init-ex7)
         idx (find-closest-centroids X centroids)
         new-centroids (map matrix (compute-centroids X idx 3))]
     (is (approx (matrix [[2.428301 3.157924] [5.813503 2.633656] [7.119387 3.616684]])
@@ -24,6 +20,6 @@
 
 (deftest test-run-kmeans
   (let [approx (approximately 1e-4)
-        {:keys [centroids X]} (init)
+        {:keys [centroids X]} (init-ex7)
         centroids (run-kmeans X centroids 10)]
     (is (approx (matrix [[1.9540 5.0256] [3.0437 1.0154] [6.0337 3.0005]]) centroids))))
